dd4a803cf7e573a45ea8a5ed4fd6e77a4079818d
Douglas CH.Z <douglash.dcz@gmail.com>
2022-06-15 13:09:47 -0400

    Se instalo la libreria webdriver_manager
    Se instalo la libreria ya mencionada, ademas se importo chrome options y chrome service

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 import time
 
 if __name__ == '__main__':
