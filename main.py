5064a61aed08368fd65760a1aefeb0311a477bf7
Douglas CH.Z <douglash.dcz@gmail.com>
2022-06-14 23:01:31 -0400

    Abrir navegador con Selenium WebDriver
    Se realizo la funcionalidad para abrir la pagina de "league of legend" en el navegador con la libreria Selenium.

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+from selenium import webdriver
 
 if __name__ == '__main__':
 
-    print('Hola mundo')
+    options = webdriver.ChromeOptions()
+    options.add_argument('--start-maximized')
+    options.add_argument('--disable-extensions')
+
+    driver_path = 'C:\\Users\\Hp\\Downloads\\chromedriver.exe'
+
+    driver = webdriver.Chrome(driver_path, chrome_options=options)
+    driver.get('https://www.leagueoflegends.com/')
