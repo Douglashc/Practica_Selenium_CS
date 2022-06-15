0a9cde3ebcad40dcec05c7e5ef44f08ee73a2f71
Douglas CH.Z <douglash.dcz@gmail.com>
2022-06-14 23:36:06 -0400

    Uso de la libreria time
    Se implemento un sleep para que el navegador espere unos segundos antes de cerrarse al terminar las pruebas.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+import time
 
 if __name__ == '__main__':
 
@@ -36,4 +37,5 @@ if __name__ == '__main__':
                                                ' ', '.')))) \
         .click()
 
+    time.sleep(20)
     driver.quit()
